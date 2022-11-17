592f14616b51f759624be1d83e209b1aecdc41cc
Raka Aditya Kuncara <116072950+RakaAdityaKuncara15@users.noreply.github.com>
2022-11-17 18:46:02 +0700

    Add files via upload

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Avatar({ user, showName }) {
     return (
         <div className="avatar-component">
-            <img className="avatar" src={user.avatar} alt="" />
+            <img className="avatar" src="https://thispersondoesnotexist.com/image" alt="" />
             {showName && <h3 className="avatar-title">{user.name}</h3>}
         </div>
     )
